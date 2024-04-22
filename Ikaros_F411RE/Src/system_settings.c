88ef109d9e6a2349c4e78c7cd8ffa570f2f3d700
sebastian godinez <sebastian.godinez@s4iot.com>
2024-04-21 22:55:08 -0600

    add drivers for handler for timers

--- Ikaros_F411RE/Src/system_settings.c
+++ Ikaros_F411RE/Src/system_settings.c
@@ -86,3 +86,27 @@ Status_code_t Delay(uint32_t Miliseconds){ //do it in microseconds later
 	return Success;
 }
 
+
+
+void timer_clock_enables(void){
+	uint32_t volatile *pClockControlReg = (uint32_t volatile *)(RCC_ADDRESS + RCC_OFFSET_APB1ENR);
+	*pClockControlReg |= (Enabled<<TIMER_2); //hasta aqui solo habilitamos el reloj del tim2
+
+	uint32_t volatile *TIM_REG_PSC = (uint32_t volatile*)(TIM2_ADDRESS + TIMx_PSC); //agregamos el 0x14 par indicar output
+//	*TIM_REG_PSC |= ;
+
+	uint32_t volatile *TIM_REG_ARR = (uint32_t volatile*)(TIM2_ADDRESS + TIMx_ARR); //agregamos el 0x14 par indicar output
+	*TIM_REG_PSC |= TIMx_DIV_BY_16;
+	NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+//uint8_t Delay_with_timer2(void){
+//
+//	//primero habilitar el reloj del timer2 APB1 con el offset del apb1
+//	//set el preescaler
+//	//set auto reload value
+//	//clear counter
+//	//enabled timer
+//
+//
+//}
--- Ikaros_F411RE/Src/system_settings.h
+++ Ikaros_F411RE/Src/system_settings.h
@@ -9,6 +9,7 @@
 #define SYSTEM_SETTINGS_H_
 
 #include <stdint.h>
+#include "stm32f4xx.h"
 
 typedef enum{
 	Success,
@@ -96,33 +97,79 @@ typedef enum{
 }memoryMapAddress_t;
 
 typedef enum{
-	RCC_OFFSET_CR =0x00,
-	RCC_OFFSET_PLLCFGR =0x04,
-	RCC_OFFSET_CFGR =0x08,
-	RCC_OFFSET_CIR = 0x0C,
-	RCC_OFFSET_AHB1RSTR =0x10,
-	RCC_OFFSET_AHB2RSTR =0x14,
-	RCC_OFFSET_APB1RSTR = 0x20,
-	RCC_OFFSET_APB2RSTR = 0x24,
-	RCC_OFFSET_AHB1ENR =0x30,
-	RCC_OFFSET_AHB2ENR =0x34,
-	RCC_OFFSET_APB1ENR =0x40,
-	RCC_OFFSET_APB2ENR =0x44,
-	RCC_OFFSET_AHB1LPENR = 0x50,
-	RCC_OFFSET_AHB2LPENR =0x54,
-	RCC_OFFSET_APB1LPENR =0x60,
-	RCC_OFFSET_APB2LPENR =0x64,
-	RCC_OFFSET_BDCR = 0x70,
-	RCC_OFFSET_CSR =0x74,
-	RCC_OFFSET_SSCGR =0x80,
-	RCC_OFFSET_PLLI2S = 0x84,
-	RCC_OFFSET_DCKCFGR =0x8C
+	RCC_OFFSET_CR =			0x00,
+	RCC_OFFSET_PLLCFGR =	0x04,
+	RCC_OFFSET_CFGR =		0x08,
+	RCC_OFFSET_CIR = 		0x0C,
+	RCC_OFFSET_AHB1RSTR =	0x10,
+	RCC_OFFSET_AHB2RSTR =	0x14,
+	RCC_OFFSET_APB1RSTR = 	0x20,
+	RCC_OFFSET_APB2RSTR = 	0x24,
+	RCC_OFFSET_AHB1ENR =	0x30,
+	RCC_OFFSET_AHB2ENR =	0x34,
+	RCC_OFFSET_APB1ENR =	0x40,
+	RCC_OFFSET_APB2ENR =	0x44,
+	RCC_OFFSET_AHB1LPENR = 	0x50,
+	RCC_OFFSET_AHB2LPENR =	0x54,
+	RCC_OFFSET_APB1LPENR =	0x60,
+	RCC_OFFSET_APB2LPENR =	0x64,
+	RCC_OFFSET_BDCR = 		0x70,
+	RCC_OFFSET_CSR =		0x74,
+	RCC_OFFSET_SSCGR =		0x80,
+	RCC_OFFSET_PLLI2S = 	0x84,
+	RCC_OFFSET_DCKCFGR =	0x8C
 
 }RCC_offset_t;
 
 typedef enum{
 	OFFSET_PORTS =0x00,
 }peripherial_offset_t;
+
+
+typedef enum{
+	TIMER_2,
+	TIMER_3,
+	TIMER_4,
+	TIMER_5
+
+}timers_enb_t;
+
+typedef enum{
+	TIMx_CR1 =0x00,
+	TIMx_CR2 =0x04,
+	TIMx_SMCR =0x08,
+	TIMx_DIER = 0x0C,
+	TIMx_SR = 0x10,
+	TIMx_EGR = 0x14,
+	TIMx_CCMR1 = 0x18,
+	TIMx_CCMR2 = 0x1C,
+	TIMx_CCER = 0x20,
+	TIMx_CNT = 0x24, //important
+	TIMx_PSC = 0x28, //important
+	TIMx_ARR = 0x2C, //important
+	TIMx_CCR1 = 0x34,
+	TIMx_CCR2 = 0x38,
+	TIMx_CCR3 =0x3C,
+	TIMx_CCR4 = 0x40,
+	TIMx_DCR = 0x48,
+	TIMx_DMAR = 0x4C,
+	TIM2_TIM5_OR = 0x50
+
+}TIM2_TIM5_register_offset_t;
+
+typedef enum{
+	TIMx_DIV_BY_1 = 	(1u<<1),
+	TIMx_DIV_BY_2 =		(1u<<2),
+	TIMx_DIV_BY_4=		(1u<<3),
+	TIMx_DIV_BY_8=		(1u<<4),
+	TIMx_DIV_BY_16=		(1u<<5),
+	TIMx_DIV_BY_32=		(1u<<6),
+	TIMx_DIV_BY_64=		(1u<<7),
+	TIMx_DIV_BY_128=	(1u<<8),
+	TIMx_DIV_BY_256=	(1u<<9),
+	TIMx_DIV_BY_512=	(1u<<10)
+
+}TIMx_preescaler_t;
 
 Status_code_t ClockEnable(Set_Port_t Port_define, Enabled_Disabled_t Intention);
 Status_code_t Delay(uint32_t Miliseconds);
